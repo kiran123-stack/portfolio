43d5db139fcf0a7173a6c689df247bbfc30452e2
Kiran <upliftverse98@gmail.com>
2025-10-10 00:22:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
     const cursor = document.getElementById("cursor");
+     const follower = document.querySelector('.cursor-follower');
+       let followerX = 0, followerY = 0;
+
 
     document.addEventListener("mousemove", (doll)=> {
           cursor.style.transform = `translate3d(${doll.clientX}px, ${doll.clientY}px, 0)`;
@@ -7,5 +10,12 @@
 
     });
         
+function animateFollower() {
+            followerX += (mouseX - followerX) * 0.1;
+            followerY += (mouseY - followerY) * 0.1;
+            follower.style.transform = `translate(${followerX}px, ${followerY}px)`;
+            requestAnimationFrame(animateFollower);
+        }
+        animateFollower();
 
 
